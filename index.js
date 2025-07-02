812704dc9949c4483f9ee4650473cadf12028ca0
Miguel Eduardo <migueled2695@gmail.com>
2025-07-01 18:19:39 -0700

    1672 de leet code

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 /*
-Input: nums = [1,2,3,4]
-Output: [1,3,6,10]
-Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Input: accounts = [[1,2,3],[3,2,1]]
+Output: 6
+Explanation:
+1st customer has wealth = 1 + 2 + 3 = 6
+2nd customer has wealth = 3 + 2 + 1 = 6
+Both customers are considered the richest with a wealth of 6 each, so return 6.
 */
-let nums = [1, 2, 3, 4];
-for (let i = 1; i < nums.length; i++) {
-    nums[i]=nums[i-1]+nums[i];
+
+var accounts = [[2, 8, 7], [7, 1, 3], [1, 9, 5]];
+console.log(accounts);
+
+function maximumWealth(accounts) {
+    var max = 0;
+    for (let i = 0; i < accounts.length; i++) {
+        var cont = 0;
+        for (let j = 0; j < accounts[0].length; j++) {
+            cont += accounts[i][j];
+        }
+        if (cont > max) {
+            max = cont
+        }
+    }
+    return max;
 }
-console.log(nums);
+
+maximumWealth(accounts);
+console.log(maximo);
